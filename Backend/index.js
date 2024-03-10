5634a0bb03399239fb0ba2692801979a09878563
Maxim Sushkov <max_123_99@mail.ru>
2024-03-11 00:21:04 +1000

    feat: added connect to mongo

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,4 @@
+// Импорт библиотек из зависимостей
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -5,11 +6,25 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
 
-// Создаем экземпляр приложентя Express
+// Импорт настроек базы данных
+const config = require('./config/db');
+
+// Создаем экземпляр приложения Express
 const app = express();
 
 // Общепринято брать 3000
 const port = 3000;
+
+// Подключение к базе данных
+mongoose.connect(config.db);
+
+mongoose.connection.on('connected', () => {
+	console.log('Successfull connection to the database');
+});
+
+mongoose.connection.on('error', err => {
+	console.log('Not successfull connection to the database');
+});
 
 // Запуск сервера
 app.listen(port, () => {
