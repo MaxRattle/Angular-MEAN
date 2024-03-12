a1978715da39f7e5f182933bc9d957d108342ebf
Maxim Sushkov <max_123_99@mail.ru>
2024-03-12 23:49:40 +1000

    feat: added header and routing in frontend

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegComponent } from './reg/reg.component';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'reg', component: RegComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
