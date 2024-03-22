9cc234888cd1b9e7378003dda652d57aff1f466f
Maxim Sushkov <max_123_99@mail.ru>
2024-03-23 01:58:10 +1000

    feat: realized part of dashboard, fixed crash, prepare to quill material

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatSelectModule } from '@angular/material/select';
+
+import { FormControl, Validators } from '@angular/forms';
+
+import { FormControlDirective } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+import { FormGroupDirective } from '@angular/forms';
+
+//
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  title = '';
+
+  selected = new FormControl('', [Validators.required]);
+
+  matcher = new MyErrorStateMatcher();
+}
